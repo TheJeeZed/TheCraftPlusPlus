172e06643ab880c691604e0c93f2286ae2da2da4
TheJeeZed <suno.anser@gmail.com>
2024-05-08 13:06:25 +0500

    CPfoods class done

--- src/main/java/net/thejeezed/craftplusplus/item/CPFoods.java
+++ src/main/java/net/thejeezed/craftplusplus/item/CPFoods.java
@@ -5,9 +5,9 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class CPFoods {
-    public static final FoodProperties RAW_SQUID_TENTICLE = new FoodProperties.Builder().nutrition(2).meat()
+    public static final FoodProperties RAW_TENTACLE = new FoodProperties.Builder().nutrition(2).meat()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 50), 0.1f).build();
-    public static final FoodProperties COOKED_SQUID_TENTICLE = new FoodProperties.Builder().nutrition(6).meat()
+    public static final FoodProperties COOKED_TENTACLE = new FoodProperties.Builder().nutrition(6).meat()
             .saturationMod(0.6f).build();
     public static final FoodProperties SEA_SOUP = new FoodProperties.Builder().nutrition(12)
             .saturationMod(0.7f).effect(() -> new MobEffectInstance(MobEffects.CONDUIT_POWER, 1200), 1).build();
--- src/main/java/net/thejeezed/craftplusplus/item/CPItems.java
+++ src/main/java/net/thejeezed/craftplusplus/item/CPItems.java
@@ -13,6 +13,11 @@ public class CPItems {
 
     public static final RegistryObject<Item> ITEM_HERE = ITEMS.register("item_here", ()-> new Item(new Item.Properties()));
 
+    //Food Section(idk why i am putting a comment
+    public static final RegistryObject<Item> RAW_TENTACLE = ITEMS.register("raw_tentacle", ()-> new Item(new Item.Properties().food(CPFoods.RAW_TENTACLE)));
+    public static final RegistryObject<Item> COOKED_TENTACLE = ITEMS.register("cooked_tentacle", ()-> new Item(new Item.Properties().food(CPFoods.COOKED_TENTACLE)));
+    public static final RegistryObject<Item> SEA_SOUP = ITEMS.register("sea_soup", ()-> new Item(new Item.Properties().food(CPFoods.SEA_SOUP)));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }

b6cbb55bb1ff306c580f297ac617c13ea802f6a7
TheJeeZed <suno.anser@gmail.com>
2024-05-08 14:35:09 +0500

    added recipes and other json files for food items. next step is to make the texture and make squid drop tentacles

--- src/main/java/net/thejeezed/craftplusplus/item/CPItems.java
+++ src/main/java/net/thejeezed/craftplusplus/item/CPItems.java
@@ -1,5 +1,6 @@
 package net.thejeezed.craftplusplus.item;
 
+import net.minecraft.world.item.BowlFoodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,9 +15,9 @@ public class CPItems {
     public static final RegistryObject<Item> ITEM_HERE = ITEMS.register("item_here", ()-> new Item(new Item.Properties()));
 
     //Food Section(idk why i am putting a comment
-    public static final RegistryObject<Item> RAW_TENTACLE = ITEMS.register("raw_tentacle", ()-> new Item(new Item.Properties().food(CPFoods.RAW_TENTACLE)));
+    public static final RegistryObject<Item> RAW_TENTACLE = ITEMS.register("raw_tentacle.json", ()-> new Item(new Item.Properties().food(CPFoods.RAW_TENTACLE)));
     public static final RegistryObject<Item> COOKED_TENTACLE = ITEMS.register("cooked_tentacle", ()-> new Item(new Item.Properties().food(CPFoods.COOKED_TENTACLE)));
-    public static final RegistryObject<Item> SEA_SOUP = ITEMS.register("sea_soup", ()-> new Item(new Item.Properties().food(CPFoods.SEA_SOUP)));
+    public static final RegistryObject<Item> SEA_SOUP = ITEMS.register("sea_soup", ()-> new BowlFoodItem(new Item.Properties().stacksTo(1).food(CPFoods.SEA_SOUP)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);

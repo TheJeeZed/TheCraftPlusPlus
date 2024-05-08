06e30b8eab51bb6067902d24b386bb7ef537d405
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-05-08 03:50:11 -0500

    Added Compressed Copper and Charcoal Block Added them to en_us.json and did the json models and blockstates the only thing left to do is png textures :D

--- src/main/java/net/thejeezed/craftplusplus/CraftPlusPlus.java
+++ src/main/java/net/thejeezed/craftplusplus/CraftPlusPlus.java
@@ -1,9 +1,11 @@
 package net.thejeezed.craftplusplus;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,6 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.thejeezed.craftplusplus.block.CPBlocks;
 import net.thejeezed.craftplusplus.item.CPItems;
 import org.slf4j.Logger;
 
@@ -22,18 +25,41 @@ public class CraftPlusPlus {
     public CraftPlusPlus() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        CPItems.register(modEventBus);
+        CPBlocks.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
-        CPItems.register(modEventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
     }
 
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS)
+        {
+            event.accept(CPBlocks.CHARCOAL_BLOCK);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        {
+            event.accept(CPBlocks.COMPRESSED_COPPER);
+        }
+    }
+
+    @SubscribeEvent
+    public void onFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event)
+    {
+        if (event.getItemStack().getItem() == CPBlocks.CHARCOAL_BLOCK.get().asItem())
+        {
+            event.setBurnTime(16000);
+        }
     }
 
     @SubscribeEvent

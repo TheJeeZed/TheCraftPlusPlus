3b74ebfa20d75029881ff15ef4fa0ae719972a83
luhcartimods <luhcartimods@gmail.com>
2024-05-11 20:29:09 +0200

    Comments For Datagen

--- src/main/java/net/thejeezed/craftplusplus/datagen/ModItemModelProvider.java
+++ src/main/java/net/thejeezed/craftplusplus/datagen/ModItemModelProvider.java
@@ -8,10 +8,14 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.armortrim.TrimMaterial;
 import net.minecraft.world.item.armortrim.TrimMaterials;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.thejeezed.craftplusplus.CraftPlusPlus;
 
 import java.util.LinkedHashMap;
 
@@ -33,6 +37,15 @@ public class ModItemModelProvider extends ItemModelProvider {
         trimmedArmorItem(TURTLE_SHELL_CHESTPLATE);
         trimmedArmorItem(TURTLE_SHELL_LEGGINGS);
         trimmedArmorItem(TURTLE_SHELL_BOOTS);
+
+        //trimmedArmorItem for trims
+        //simpleItem for a simple item. This will make it generated
+        //evenSimplerBlockItem this is for blocks to make a item model.
+        //trapdoorItem for trapdoors
+        //fenceItem for fences
+        //buttonItem for buttons
+        //handheldItem anything weilded, like with a stick
+        //simpleBlockItem block
     }
 
     private void trimmedArmorItem(RegistryObject<Item> itemRegistryObject) {
@@ -89,4 +102,47 @@ public class ModItemModelProvider extends ItemModelProvider {
             });
         }
     }
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(CraftPlusPlus.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    public void evenSimplerBlockItem(RegistryObject<Block> block) {
+        this.withExistingParent(CraftPlusPlus.MOD_ID + ":" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath()));
+    }
+
+    public void trapdoorItem(RegistryObject<Block> block) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
+                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath() + "_bottom"));
+    }
+
+    public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
+                .texture("texture",  new ResourceLocation(CraftPlusPlus.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    public void buttonItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
+                .texture("texture",  new ResourceLocation(CraftPlusPlus.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    public void wallItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/wall_inventory"))
+                .texture("wall",  new ResourceLocation(CraftPlusPlus.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/handheld")).texture("layer0",
+                new ResourceLocation(CraftPlusPlus.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    private ItemModelBuilder simpleBlockItem(RegistryObject<Block> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(MOD_ID,"item/" + item.getId().getPath()));
+    }
+}
 }

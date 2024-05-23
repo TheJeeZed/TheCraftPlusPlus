7d5bea10161eb68834fac5c30872c270b102debc
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-05-23 13:44:51 -0500

    Removed cooldown when in creative mode

--- src/main/java/net/thejeezed/craftplusplus/item/custom/item/MagicMirrorItem.java
+++ src/main/java/net/thejeezed/craftplusplus/item/custom/item/MagicMirrorItem.java
@@ -1,20 +1,16 @@
 package net.thejeezed.craftplusplus.item.custom.item;
 
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
-import net.thejeezed.craftplusplus.client.gui.MessageRenderer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
@@ -26,9 +22,12 @@ public class MagicMirrorItem extends Item {
 
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, @NotNull Player player, @NotNull InteractionHand pUsedHand) {
-        if (!pLevel.isClientSide && player instanceof ServerPlayer serverPlayer) {
+        if (!pLevel.isClientSide && player instanceof ServerPlayer serverPlayer)
+        {
             ServerLevel world = (ServerLevel) pLevel;
-            if (!player.getAbilities().instabuild){
+
+            if (!player.getAbilities().instabuild && !player.isCreative())
+            {
                 player.getCooldowns().addCooldown(this, 1200);
             }
 
@@ -54,7 +53,11 @@ public class MagicMirrorItem extends Item {
                                 serverPlayer.getXRot()
                         );
 
-                        serverPlayer.getCooldowns().addCooldown(this, 200);
+                        if (!player.isCreative())
+                        {
+                            serverPlayer.getCooldowns().addCooldown(this, 200);
+                        }
+
                         player.getItemInHand(pUsedHand).hurtAndBreak(1, player,
                                 player1 -> player.broadcastBreakEvent(player.getUsedItemHand()));
                     }
